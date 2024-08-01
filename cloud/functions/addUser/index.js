ab55bc02f34a867950f4a1ad568114d6fb382951
刘语 <liuyu@liuyudeMacBook-Pro.local>
2024-08-01 11:48:05 +0800

    save

--- cloud/functions/addUser/index.js
+++ cloud/functions/addUser/index.js
@@ -2,9 +2,8 @@
 const cloud = require('wx-server-sdk');
 
 // 初始化云开发环境
-cloud.init({
-    env: cloud.DYNAMIC_CURRENT_ENV
-  });
+cloud.init();
+
 // 获取数据库引用
 const db = cloud.database();
 const _ = db.command;
@@ -12,7 +11,7 @@ const _ = db.command;
 // 云函数入口函数
 exports.main = async (event, context) => {
     const wxContext = cloud.getWXContext();
-    const { OPENID } = wxContext;
+    const OPENID = wxContext.OPENID || wxContext.FROM_OPENID;
 
     const transaction = await db.startTransaction();
 
