497f20a4b6d4be0c3bc1655600944d5b2f5d326c
刘语 <liuyu@liuyudeMacBook-Pro.local>
2024-09-11 08:29:26 +0800

    save

--- client/src/pages/index/index.jsx
+++ client/src/pages/index/index.jsx
@@ -380,31 +380,6 @@ const CameraPage = () => {
   };
   let interstitialAd = null;
   useEffect(() => {
-    // 若在开发者工具中无法预览广告，请切换开发者工具中的基础库版本
-    // 在页面中定义插屏广告
-
-    // 在页面onLoad回调事件中创建插屏广告实例
-    if (userInfo.type === "default") {
-      if (wx.createInterstitialAd) {
-        interstitialAd = wx.createInterstitialAd({
-          adUnitId: "adunit-58e7bd94d036305e",
-        });
-        interstitialAd.onLoad(() => {});
-        interstitialAd.onError((err) => {
-          console.error("插屏广告加载失败", err);
-        });
-        interstitialAd.onClose(() => {
-          setAddAnimate(true);
-        });
-      }
-
-      // 在适合的场景显示插屏广告
-      if (interstitialAd) {
-        interstitialAd.show().catch((err) => {
-          console.error("插屏广告显示失败", err);
-        });
-      }
-    }
     checkPermissions();
     requestPermission();
   }, []);
