5ab08e38897a47f54f6c8e206a7576f2ee8a7a77
jason <jason_story@163.com>
2024-11-13 22:30:29 +0800

    save

--- client/src/appConfig.js
+++ client/src/appConfig.js
@@ -48,6 +48,20 @@ const appConfigs = {
     containerName: "express-ptcb",
     userToApp: "子小程序",
   },
+  wx6f34e2a245c3a2d1: {
+    // 自定义水印照片 ly 主小程序
+    type: "shared",
+    resourceAppid: "wx8d21222bcb51c801",
+    resourceEnv: "ly-9gjnymq6d9d7ca23",
+    priceShow: "isSecondShow",
+    priceMap: "jiage2",
+    notify_url: "https://1330414900-iztv1ielys.ap-shanghai.tencentscf.com",
+    containerId: "prod-5gzgeq4v5542b918",
+    containerResourceAppid: "wx8d21222bcb51c801",
+    containerResourceEnv: "prod-5gzgeq4v5542b918",
+    containerName: "express-ptcb",
+    userToApp: "子小程序",
+  },
 };
 
 export { appConfigs };
