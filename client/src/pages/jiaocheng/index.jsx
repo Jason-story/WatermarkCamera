b071b1487c433f7808276deeae75a4b34634997c
jason <jason_story@163.com>
2024-11-22 22:00:33 +0800

    save

--- client/src/pages/jiaocheng/index.jsx
+++ client/src/pages/jiaocheng/index.jsx
@@ -3,12 +3,13 @@ import { View, Image, Button, Text } from "@tarojs/components";
 
 import "./index.scss";
 const app = getApp();
-
+const imgs = ["1.png", "2.png", "3.png", "4.png", "5.png"];
+const prefix = "https://gitee.com/jasonstory/fonts/raw/master/img/";
 const QRCodePage = () => {
   const config = app.$app.globalData.config;
   return (
     <View className="qr-code-page">
-      <View className="user-details">
+      {/* <View className="user-details">
         <View>1. 先点击首页-修改，选择水印</View>
         <View>2. 点击编辑或者直接点击水印</View>
         <View>3. 修改时间、地点、水印名字、打卡标签等信息</View>
@@ -17,7 +18,7 @@ const QRCodePage = () => {
         <View>
           6. 如果是视频水印，请先用手机原相机拍摄一段视频，然后再从相册选择即可
         </View>
-      </View>
+      </View> */}
       {/* {!app.$app.globalData.fuckShenHe && (
         <View className="user-details">
           {config.jiaochengtext.map((item, index) => {
@@ -37,15 +38,15 @@ const QRCodePage = () => {
         </View>
       )} */}
       <View className="qr-code-container">
-        {config.jiaocheng_image.map((item, index) => (
+        {imgs.map((item, index) => (
           <View key={index} className="qr-code-item">
             <Image
-              src={item}
+              src={prefix + item}
               className="qr-code-image"
               showMenuByLongpress={true}
               style={{
                 width: "calc(45vw)",
-                height: "calc(45vw * 2.17)",
+                height: "calc(45vw * 1.778)",
                 margin: "10px 0",
               }}
             />
